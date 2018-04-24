7d7dfb621059c0ae7e9b13c89107fc24a1c27795
Giang Dinh <giangdinhtt@gmail.com>
2018-04-24 17:46:18 +0700

    Add config for FB messenger webhook

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,9 @@ module.exports = {
         COMMON_VARIABLE: 'true'
       },
       env_production : {
-        NODE_ENV: 'production'
+        NODE_ENV: 'production',
+        MESSENGER_VERIFY_TOKEN: '25e838ce-7524-45c5-b6df-49a7a7430d7f',
+        MESSENGER_ACCESS_TOKEN: 'EAADKZBNefPDIBABIgImuBTwV7X2c6vG1GZBRaW6hlKVCfZCie9nMiHSDeWY9FFrvSenZA4HXRzGvTPmEPKE1wlVGgBsrvUcV4gTFtxKAS7f8ujlk2QjzHPGScrQju5r1V9Gq09BNqs5CdMlEWe9UhnBnrpumkoiy1BPftDrvsgZDZD'
       }
     }
   ]
