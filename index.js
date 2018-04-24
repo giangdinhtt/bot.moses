3a6f4e0e330315d55fd776e25d579524a5905181
Giang Dinh <giang.dinh@tiki.vn>
2018-04-24 17:44:15 +0700

    Config FB messenger

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ const Bottr = require('bottr')
 const BottrApp = require('bottr-app')
 const bot = new Bottr.Bot()
 
-bot.use(new Bottr.FacebookMessengerClient())
+bot.use(new BottrApp())
+//bot.use(new Bottr.FacebookMessengerClient())
+
+bot.on('message_received', function(message, session) {
+  session.send('Tôi có thể giúp gì được cho bạn?')
+})
+
 bot.listen()
